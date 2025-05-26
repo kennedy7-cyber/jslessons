96746f6c5e85290ca1858498a1e6064e6f02ecd9
kennedy <adichronix@kali.andy>
2025-05-26 21:02:52 +0300

    learned about the javascript loops

---
+++ forLoop.js
@@ -0,0 +1,9 @@
+// function myFunction()I
+for(let i=5; i>=1; i--){
+    console.log('hello world',i);
+}
+
+let modulusCHeck;
+modulusCHeck = 10
+
+console.log('object of the modulus check', modulusCHeck);
